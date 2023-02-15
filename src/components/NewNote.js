3885cb65ed847508520f11459ec9962524d5fe57
Mrunmayee Gaikwad <mrunmayee2906@gmail.com>
2023-02-15 16:31:10 +0530

    Restore NewNote where form-hook is not used

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,45 +1,22 @@
-import { useState, useCallback, useReducer } from "react";
+import { useState } from "react";
 
-import Input from "./FormElements/Input";
-import { VALIDATOR_REQUIRE } from "../util/validators";
-import { useForm } from "../hooks/form-hooks";
 import "./NewNote.css";
 
 const NewNote = (props) => {
-  // const [inputNote, setInputNote] = useState({
-  //   title: "",
-  //   content: "",
-  // });
-  // // const [inputContent, setInputContent] = useState("");
+  const [inputNote, setInputNote] = useState({
+    title: "",
+    content: "",
+  });
+  // const [inputContent, setInputContent] = useState("");
 
-  // const handleChange = (event) => {
-  //   // console.log(event.target.value);
-  //   const { value, name } = event.target;
+  const handleChange = (event) => {
+    // console.log(event.target.value);
+    const { value, name } = event.target;
 
-  //   setInputNote((prevNote) => ({
-  //     ...prevNote,
-  //     [name]: value,
-  //   }));
-  // };
-
-  const [formState, inputHandler] = useForm(
-    {
-      // title and content together
-      title: {
-        value: "",
-        isValid: false,
-      },
-      content: {
-        value: "",
-        isValid: false,
-      },
-    },
-    false
-  );
-
-  const noteSubmitHandler = (event) => {
-    event.preventDefault();
-    console.log(formState.inputs);
+    setInputNote((prevNote) => ({
+      ...prevNote,
+      [name]: value,
+    }));
   };
 
   return (
@@ -47,46 +24,28 @@ const NewNote = (props) => {
       {/* set a callback for submit, which takes the current note as input */}
       <form
         className="create-note"
-        // onSubmit={(event) => {
-        //   props.onSubmit(event, inputNote);
-        //   setInputNote({
-        //     title: "",
-        //     content: "",
-        //   });
-        // }}
-        onSubmit={noteSubmitHandler}
+        onSubmit={(event) => {
+          props.onSubmit(event, inputNote);
+          setInputNote({
+            title: "",
+            content: "",
+          });
+        }}
       >
-        <Input
-          id="title"
-          element="input"
-          type="text"
-          placeholder="Title"
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Please enter valid email id"
-          onInput={inputHandler}
-        />
-        <Input
-          id="content"
-          element="textarea"
-          placeholder="Take a note..."
-          validators={[VALIDATOR_REQUIRE()]}
-          errorText="Password length should be min 6 characters"
-          onInput={inputHandler}
-        />
         <input
           name="title"
           placeholder="Title"
-          // onChange={handleChange}
-          // value={inputNote.title}
+          onChange={handleChange}
+          value={inputNote.title}
         />
         <textarea
           name="content"
           placeholder="Take a note..."
           rows="3"
-          // onChange={handleChange}
-          // value={inputNote.content}
+          onChange={handleChange}
+          value={inputNote.content}
         />
-        <button type="submit" className="add-btn" disabled={!formState.isValid}>
+        <button type="submit" className="add-btn">
           Add
         </button>
       </form>
